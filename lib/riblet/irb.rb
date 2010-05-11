7e6d5e85cbec6cbe10bcb4f8ef943d44301b2fd5
Luke Pillow <lpillow@gmail.com>
2010-05-11 16:46:23 -0500

    Added the ability to list loaded riblets in irb

--- lib/riblet/irb.rb
+++ lib/riblet/irb.rb
@@ -5,6 +5,14 @@ module Riblet
       
       puts "** Loaded: #{riblet.to_s(:usage)}" if riblet && riblet.loaded
     end
+
+    def riblets(type=nil)
+      puts "Using #{Riblet.addons.size} riblet(s)...\n\n"
+
+      Riblet.addons.each do |addon|
+        puts addon.to_s(type)
+      end
+      nil
     end
   end
 end
