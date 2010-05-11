a51315d326763f8ff60f0bc31d119a9d0ba668c7
Luke Pillow <lpillow@gmail.com>
2010-05-11 13:30:34 -0500

    Added @@addons module accessor to keep track of installed addons/riblets

--- lib/riblet.rb
+++ lib/riblet.rb
@@ -1,3 +1,14 @@
+module Riblet
+  @@addons = []
+  def self.addons
+    @@addons
+  end
+
+  def self.addons=(riblet)
+    @@addons << riblet
+  end
+end
+
 require 'riblet/add_on'
 require 'riblet/irb'
 
